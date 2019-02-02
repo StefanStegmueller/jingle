b8c4abf3f4679ed5f458524f88126737cb395dc6
GordonShumway <Stefan.Steg@gmail.com>
2019-02-02 18:28:49 +0100

    -add: duty cycle as cli argument

--- src/config.rs
+++ src/config.rs
@@ -1,19 +1,24 @@
 pub struct Config {
     pub filename: String,
+    pub duty_cycle: u32,
 }
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 2 {
+        if args.len() < 3 {
             return Err("not enough arguments");
         }
 
-        if args.len() > 2 {
+        if args.len() > 3 {
             return Err("too much arguments");
         }
 
         let filename = args[1].clone();
+        let duty_cycle = args[2].clone().parse::<u32>().unwrap();
 
-        Ok(Config { filename })
+        Ok(Config {
+            filename,
+            duty_cycle,
+        })
     }
 }
